fb208aae924b955ae7d3da2bb2125e687a1e5710
Faizan Gillani <165652722+faizan-gp@users.noreply.github.com>
2025-09-17 02:26:53 +0800

    include robot.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://zlwebster.com",
-  generateRobotsTxt: false,
+  generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: "daily",
   priority: 0.7,
