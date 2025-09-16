eb3638a1ad1c6da850acbf37f7d61cf39c7231db
Faizan Gillani <165652722+faizan-gp@users.noreply.github.com>
2025-09-17 02:23:04 +0800

    revert sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://zlwebster.com",
-  generateRobotsTxt: true,   // will auto-generate robots.txt
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
   changefreq: "daily",
   priority: 0.7,
-  sitemapSize: 10000,        // effectively disables splitting
-  generateIndexSitemap: false, // ensures only one sitemap.xml (no sitemap-0.xml)
 };
