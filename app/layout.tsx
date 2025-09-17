661db82010d178c3fa21f068a3724a092956bac5
Faizan Gillani <165652722+faizan-gp@users.noreply.github.com>
2025-09-18 01:19:19 +0800

    fix meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -150,7 +150,6 @@ export default function RootLayout({
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '${META_PIXEL_ID}');
-                fbq('track', 'PageView');
               `}
             </Script>
             <noscript>
