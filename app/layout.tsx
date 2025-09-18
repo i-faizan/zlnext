b19647921c0809362c929191f8156cf6e8b8f44d
Faizan Gillani <165652722+faizan-gp@users.noreply.github.com>
2025-09-18 09:53:49 +0800

    removed meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -136,33 +136,6 @@ export default function RootLayout({
             </Script>
           </>
         )}
-
-        {META_PIXEL_ID && (
-          <>
-            <Script id="fb-pixel" strategy="afterInteractive">
-              {`
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${META_PIXEL_ID}');
-              `}
-            </Script>
-            <noscript>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img height="1" width="1" style={{ display: 'none' }}
-                src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
-                alt="" />
-            </noscript>
-          </>
-        )}
-
-
-
       </head>
       <body className={`${poppins.variable} ${montserrat.variable} antialiased`}>
         <a href="#main-content" className="sr-only focus:not-sr-only">Skip to content</a>
